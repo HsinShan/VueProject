2b7918d140a7640aca7f19e704814a6b9809d119
謝欣珊 <saps920030@gmail.com>
2018-08-27 15:18:46 +0800

    blog1

---
+++ Blog1/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <blogadd></blogadd>
+  </div>
+</template>
+
+<script>
+import blogadd from './components/blogadd.vue';
+export default {
+  components:{
+    'blogadd':blogadd
+  },
+  data () {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ Blog1/src/components/blogadd.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="blog-add">
+    <h1>New Blog Post</h1>
+    <form>
+       <label for="title">Blog title:</label>
+       <input type="text" v-model.lazy="name" required><br>
+       <label for="content">Blog content</label>
+       <textarea v-model.lazy="content"></textarea>
+    </form>
+    <div id="preview">
+        <h2>Preview</h2>
+        <p>Blog title:{{name}}</p>
+        <p>Blog content:</p>
+        <p>{{content}}</p>
+    </div>
+    
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+        name:"",
+        content:""
+      }
+  }
+};
+</script>
+
+<style scoped>
+#blog-add *{
+    box-sizing: border-box;
+}
+#blog-add{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px;
+}
+input[type="text"],textarea{
+    width: 100%;
+    display: block;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px #ddd solid;
+    margin: 20px auto;
+}
+</style>
